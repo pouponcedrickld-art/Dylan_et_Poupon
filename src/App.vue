1d7e1e72aa83063ba6f6c05906814ee90bfa112b
POUPON CEDRIC KOULEDOU <cedric.kouledou@chem.loc>
2026-02-12 16:05:51 +0100

    Add home page et blog page

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,40 @@
-<script setup></script>
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+
+const routes = [
+  {path:'/blog' , title: "Blog"},
+  {path:'/' , title: "home"},
+]
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+<nav>
+
+  <RouterLink to="/">home</RouterLink>
+  <RouterLink to="/blog"> Blog</RouterLink>
+</nav>
+<RouterView ></RouterView>
 </template>
 
-<style scoped></style>
+<style scoped>
+nav {
+  display: flex;
+  gap: 15px;
+  padding: 20px;
+  background: #f4f4f4;
+  display: flex;
+  justify-content: center;
+}
+
+a {
+  text-decoration: none;
+  color: #333;
+  font-weight: bold;
+}
+
+a.router-link-active {
+  color: #42a1b8;
+  /* border-bottom: 2px solid #428db88c; */
+}
+</style>
+
